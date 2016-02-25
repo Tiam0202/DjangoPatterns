8fb070b17a9c2c5cba07f440af412f575cf77d9a
tiam0202 <tiam0202@github.com>
2016-02-25 23:52:14 +0300

    add .idea folder todo for main urls

--- DjangoPatterns/urls.py
+++ DjangoPatterns/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+# TODO: Organize humanoids url paths
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^patterns/', include('patterns.urls')),
